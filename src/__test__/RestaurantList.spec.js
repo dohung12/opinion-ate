5200148f612d0ba83a415bbfcaf9411ec9e96a62
dohung <dohuyhung12@gmail.com>
2022-02-03 15:50:36 +0700

    remove duplicate code in in test file

--- src/__test__/RestaurantList.spec.js
+++ src/__test__/RestaurantList.spec.js
@@ -2,26 +2,31 @@ import { render, screen } from '@testing-library/react'
 import { RestaurantList } from '../components/RestaurantList'
 
 describe('Restaurant List', () => {
-  it('should load restaurants on first render', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants')
-
+  const restaurants = [
+    { id: 1, name: 'Sushi Place' },
+    { id: 2, name: 'Pizza Place' },
+  ]
+  let loadRestaurants
+  const context = () => {
     render(
-      <RestaurantList loadRestaurants={loadRestaurants} restaurants={[]} />
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />
     )
+  }
 
+  beforeEach(() => {
+    loadRestaurants = jest.fn().mockName('loadRestaurants')
+  })
+
+  it('should load restaurants on first render', () => {
+    context()
     expect(loadRestaurants).toHaveBeenCalled()
   })
 
   it('should display the restaurants', () => {
-    const noop = () => {}
-
-    const restaurants = [
-      { id: 1, name: 'Sushi Place' },
-      { id: 2, name: 'Pizza Place' },
-    ]
-
-    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />)
-
+    context()
     expect(screen.getByText('Sushi Place')).not.toBeNull()
     expect(screen.getByText('Pizza Place')).not.toBeNull()
   })
