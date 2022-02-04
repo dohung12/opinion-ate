5147e4df0aa27db28bd8c43211c016ffd379f9ac
dohung <dohuyhung12@gmail.com>
2022-02-04 15:57:08 +0700

    creating api client

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,6 @@ import { devToolsEnhancer } from 'redux-devtools-extension'
 import rootReducer from './reducers'
 import thunk from 'redux-thunk'
 import api from '../api'
-import axios from 'axios'
-
-const client = axios.create({
-  baseURL:
-    'https://outside-in-dev-api.herokuapp.com/49mFIdtKfIzAsR0jYcxbEpP22b2p13pR',
-})
 
 const store = createStore(
   rootReducer,
